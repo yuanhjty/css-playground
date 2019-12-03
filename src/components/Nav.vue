df0953135d62a6acc9d51a415812762149f6eaf4
yuanhjty <yuanhjty@163.com>
2019-12-03 09:20:30 +0800

    change styles of Nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="nav">
-    <router-link v-for="route in routes" :key="route.path" :to="route.path">{{
-      route.name
-    }}</router-link>
+    <router-link v-for="route in routes" :key="route.path" :to="route.path">
+      {{ route.name }}
+    </router-link>
   </div>
 </template>
 
@@ -30,18 +30,22 @@ export default {
   display: block;
   font-weight: bold;
   color: #2c3e50;
-  padding: 8px 16px;
+  white-space: nowrap;
+  min-width: min-content;
   text-decoration: none;
+  padding: 8px 16px;
   border-bottom: solid 1px #fff;
 }
 
 .nav a:hover {
-  color: rgba(66, 185, 131, 0.5);
+  color: rgba(66, 185, 131, 1);
   background: rgba(231, 231, 231, 0.5);
 }
 
 .nav a.router-link-exact-active {
   color: #42b983;
   background: #e7e7e7;
+  border-left: solid 6px #42b983;
+  padding-left: 10px;
 }
 </style>
