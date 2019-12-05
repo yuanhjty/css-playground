1473d1f4caa6fab7dc7903e78e50e5d08be7cd12
yuanhjty <yuanhjty@163.com>
2019-12-05 10:53:04 +0800

    refactor: resizable example rendering container

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="example">
-    <p class="code-title" v-if="title">{{ title }}</p>
+    <p class="title" v-if="title">{{ title }}</p>
     <hr />
-    <div class="panel" ref="panel">
+    <p class="title">rendering result</p>
+    <Resizable panelColor="#1ec0ff" initialWidth="100%">
       <div class="result">
         <slot></slot>
       </div>
-      <div class="right-handler" @mousedown.capture.prevent="startResize"></div>
-    </div>
-    <div class="code-title" v-if="html">html</div>
+    </Resizable>
+    <div class="title" v-if="html">html</div>
     <div class="code" v-if="html">{{ html }}</div>
-    <div class="code-title" v-if="css">css</div>
+    <div class="title" v-if="css">css</div>
     <div class="code" v-if="css">{{ css }}</div>
   </div>
 </template>
 
 <script>
+import Resizable from "./Resizable";
+
 export default {
   name: "Example",
+  components: {
+    Resizable
+  },
   props: {
     title: {
       type: String,
@@ -31,55 +36,12 @@ export default {
       type: String,
       default: ""
     }
-  },
-  data() {
-    return {
-      mouseXOnStartResize: 0,
-      panelMarginRightOnStartResize: 0
-    };
-  },
-  methods: {
-    startResize(e) {
-      const panelEl = this.$refs.panel;
-      if (!panelEl) {
-        return;
-      }
-
-      const currentMarginRight = parseInt(panelEl.style.marginRight, 10) || 0;
-      this.panelMarginRightOnStartResize = currentMarginRight;
-      this.mouseXOnStartResize = e.x;
-
-      document.addEventListener("mousemove", this.resize, false);
-      document.addEventListener("mouseup", this.endResize, false);
-    },
-    endResize() {
-      document.removeEventListener("mousemove", this.resize, false);
-      document.removeEventListener("mouseup", this.endResize, false);
-    },
-    resize(e) {
-      const dx = e.x - this.mouseXOnStartResize;
-      if (dx === 0) {
-        return;
-      }
-
-      requestAnimationFrame(() => {
-        const panelEl = this.$refs.panel;
-        if (!panelEl) return;
-
-        const targetMarginRight = this.panelMarginRightOnStartResize - dx;
-        if (panelEl.offsetWidth < 0 || targetMarginRight < 0) {
-          return;
-        }
-
-        panelEl.style.marginRight = `${targetMarginRight}px`;
-      });
-    }
   }
 };
 </script>
 
 <style scoped>
-.code-title {
+.title {
   font-family: Courier, monospace;
 }
 
@@ -93,24 +55,9 @@ export default {
   overflow: auto;
 }
 
-.panel {
-  position: relative;
-  margin: 16px 0;
-  padding-right: 5px;
-  background: #f5f5f5;
-}
-
-.right-handler {
-  width: 5px;
-  height: 100%;
-  position: absolute;
-  right: 0;
-  top: 0;
-  background: #1ec0ff;
-  cursor: col-resize;
-}
-
 .result {
-  padding: 16px 0;
+  padding: 12px 20px;
+  margin: 12px 0;
+  background: #f5f5f5;
 }
 </style>
