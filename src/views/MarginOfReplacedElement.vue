a774d207fd2ae4bf6502cf3d810e8b85bb9b1f54
yuanhjty <yuanhjty@163.com>
2019-12-06 15:41:25 +0800

    margin of replaced element

--- src/views/MarginOfReplacedElement.vue
+++ src/views/MarginOfReplacedElement.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <h1>内联替换元素的垂直margin</h1>
-    <p>内联非替换元素的垂直margin无效，但内联替换元素的垂直margin有效。</p>
+    <h1>内联替换元素的margin</h1>
+    <p>内联非替换元素的margin无效，但内联替换元素的margin有效。</p>
 
     <Example :html="html" :css="css">
       <div>
-        <img class="vertival-margin" src="https://placekitten.com/200/150" />
+        <img class="margin" src="https://placekitten.com/200/150" />
       </div>
       <div>
-        <input class="vertical-margin" />
+        <input class="margin" />
       </div>
     </Example>
   </div>
@@ -16,8 +16,8 @@
 
 /************************/
 <style scoped>
-.vertical-margin {
-  margin: 40px 0;
+.margin {
+  margin: 40px;
 }
 </style>
 
